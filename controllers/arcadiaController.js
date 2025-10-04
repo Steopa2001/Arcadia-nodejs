b4194bf58b2cbd0a17b806585bed91c39195fd22
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-10-04 18:38:59 +0200

    [add] feat indexGiochiTavolo

--- controllers/arcadiaController.js
+++ controllers/arcadiaController.js
@@ -20,6 +20,16 @@ const indexCategories = (req, res) => {
   })
 }
 
+const indexGiochiTavolo = (req, res) => {
+  const sql = 'SELECT * FROM products WHERE category_id = ?'
+
+  connection.query(sql, [1], (err, result) => {
+    if (err) return res.status(500).json('Errore nella esecuzione della query: ' + err)
+
+    res.send(result);
+  })
+}
+
 module.exports = {
   indexProducts,
   indexCategories
