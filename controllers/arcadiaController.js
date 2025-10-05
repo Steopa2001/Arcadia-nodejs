fcc37e9eae933e08d363c3169ed8f9f10aed70c0
Marco Bacci <marcobacci94@gmail.com>
2025-10-05 13:48:50 +0200

    [FEAT] add show, add post, add delete

--- controllers/arcadiaController.js
+++ controllers/arcadiaController.js
@@ -1,36 +1,136 @@
-const connection = require('../data/db')
+const connection = require("../data/db");
 
 const indexProducts = (req, res) => {
-  const sql = 'SELECT * FROM products'
+  const sql = "SELECT * FROM products";
 
   connection.query(sql, (err, result) => {
-    if (err) return res.status(500).json('Errore nella esecuzione della query: ' + err)
+    if (err)
+      return res
+        .status(500)
+        .json("Errore nella esecuzione della query: " + err);
 
     res.send(result);
-  })
-}
+  });
+};
 
+// ............... PRODUCTS SHOW......................
+const showProducts = (req, res) => {
+  const { id } = req.params;
+  const sql = "SELECT * FROM products WHERE id = ?";
+
+  connection.query(sql, [id], (err, resultProduct) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "errore nell'esecuzione della query: " + err });
+    }
+
+    if (!resultProduct || resultProduct.length === 0) {
+      return res.status(404).json({ error: "Product not Found" });
+    }
+
+    return res.json(resultProduct[0]);
+  });
+};
+
+//  ----------------------PRODUCTS POST-------------------------------------
+
+// POST /products
+const createProduct = (req, res) => {
+  const {
+    name,
+    slug,
+    price,
+    description,
+    discount,
+    player_number,
+    complexity,
+    language,
+    duration,
+    recommended_age,
+    genre,
+    image,
+    category_id,
+  } = req.body;
+
+  if (!name) return res.status(400).json({ message: "il nome è obbligatorio" });
+
+  const sql = `
+    INSERT INTO products
+      (name, slug, price, description, discount, player_number, complexity, language, duration, recommended_age, genre, image, category_id)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
+  `;
+
+  connection.query(
+    sql,
+    [
+      name,
+      slug,
+      price,
+      description,
+      discount,
+      player_number,
+      complexity,
+      language,
+      duration,
+      recommended_age,
+      genre,
+      image,
+      category_id,
+    ],
+    (err, result) => {
+      if (err) return res.status(500).json({ error: "Errore INSERT: " + err });
+      res.status(201).json({
+        id: result.insertId,
+        name,
+        slug,
+        price,
+        description,
+        discount,
+        player_number,
+        complexity,
+        language,
+        duration,
+        recommended_age,
+        genre,
+        image,
+        category_id,
+      });
+    }
+  );
+};
+
+// ----------------------- DELETE PRODUCT----------------------------------------
+
+const deleteProduct = (req, res) => {
+  const { id } = req.params;
+  const sql = "DELETE FROM products WHERE id = ?";
+
+  connection.query(sql, [id], (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Errore DELETE: " + err });
+    }
+
+
+    res.json({ message: `Product ${id} eliminato con successo` });
+  });
+};
+
+// --------------------CATEGORIES INDEX------------------------------------------
 const indexCategories = (req, res) => {
-  const sql = 'SELECT * FROM categories'
-
+  const sql = "SELECT * FROM categories";
   connection.query(sql, (err, result) => {
-    if (err) return res.status(500).json('Errore nella esecuzione della query: ' + err)
-
-    res.send(result);
-  })
-}
-
-const indexGiochiTavolo = (req, res) => {
-  const sql = 'SELECT * FROM products WHERE category_id = ?'
-
-  connection.query(sql, [1], (err, result) => {
-    if (err) return res.status(500).json('Errore nella esecuzione della query: ' + err)
-
-    res.send(result);
-  })
-}
+    if (err) return res.status(500).json({ error: "Errore query: " + err });
+    res.json(result);
+  });
+};
 
 module.exports = {
   indexProducts,
-  indexCategories
-}
+  showProducts,
+  createProduct, 
+  deleteProduct,
+  indexCategories,
+};
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -5,5 +5,8 @@ const router = express.Router()
 const arcadiaController = require('../controllers/arcadiaController')
 
 router.get('/', arcadiaController.indexProducts)
+router.get('/:id', arcadiaController.showProducts);
+router.post("/", arcadiaController.createProduct);
+router.delete("/:id", arcadiaController.deleteProduct);
 
 module.exports = router
